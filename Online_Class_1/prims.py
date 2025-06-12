77b9745ec3b74ebe4e4a9288a5d1698ecc6f2553
Musfique Ahmed <anikmushfik@gmail.com>
2025-06-13 00:56:30 +0600

    Update prims.py

--- Online_Class_1/prims.py
+++ Online_Class_1/prims.py
@@ -6,6 +6,9 @@ with open("C:/Musfique's Folder/Python/DSA-2-Lab/Online_Class_1/sparseGraph.txt"
     # hi = f.readline()
     # print(repr(hi))
     # print(f.readline().split())
+    # 1. Read the next line: f.readline() -> "5 10\n"
+    # 2. Split the string into a list: .split() -> ['5', '10']
+    # 3. Map the int function to each item in the list -> (an iterator for 5 and 10)
     n, m = map(int, f.readline().split())
 
     print(n,m)
