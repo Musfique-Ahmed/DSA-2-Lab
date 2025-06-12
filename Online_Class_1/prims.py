eb36f372231d9cc4f5808d0625d9ce5760e7c4f8
Musfique Ahmed <anikmushfik@gmail.com>
2025-06-13 00:54:03 +0600

    Update prims.py

--- Online_Class_1/prims.py
+++ Online_Class_1/prims.py
@@ -3,8 +3,9 @@
 # n = no of vertex, m = no of edge
 
 with open("C:/Musfique's Folder/Python/DSA-2-Lab/Online_Class_1/sparseGraph.txt", "r") as f:
-    hi = f.readline()
-    print(repr(hi))
-    n, m = map(int, f.readline())
+    # hi = f.readline()
+    # print(repr(hi))
+    # print(f.readline().split())
+    n, m = map(int, f.readline().split())
 
     print(n,m)
