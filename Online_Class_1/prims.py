f50b5333a4118bf6e78734af2530a9387ce33cc9
Musfique Ahmed <anikmushfik@gmail.com>
2025-06-19 01:08:07 +0600

    kare

--- Online_Class_1/prims.py
+++ Online_Class_1/prims.py
@@ -1,6 +1,32 @@
+import heapq
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def primsAlgorithm(adjlist, root):
+    parentArray = [-1]  * len(adjacency_list)
+    key = [float('inf')] * len(adjacency_list)
+    treeVertex = [False] * len(adjacency_list)
+
+    key[root] = 0
+
+    minHeap = [(key[root],root)] #(key, vertex)
+
+    while minHeap:
+        minkey , u = heapq.heappop(minHeap)
+        treeVertex[u] = True
+
+        for v, w in adjacency_list[u]:
+            if not treeVertex[v] and key[v] > w:
+                key[v] = w
+                parentArray[v] = u
+                heapq.heappush(minHeap, (key[v], v))
+
+    return key, parentArray
 
 
-# n = no of vertex, m = no of edge
+
+
+# n = no of vertex, m = no of edge (50034)
 
 with open("C:/Musfique's Folder/Python/DSA-2-Lab/Online_Class_1/sparseGraph.txt", "r") as f:
     # hi = f.readline()
@@ -13,5 +39,29 @@ with open("C:/Musfique's Folder/Python/DSA-2-Lab/Online_Class_1/sparseGraph.txt"
 
     # print(n,m)
 
-    adjacency_matrix = {i:[] for i in range(n)}
-    print(adjacency_matrix)
+    adjacency_list = {i:[] for i in range(n)}
+    # print(adjacency_list)
+
+    for _ in range(m):
+        u,v,w = map(int, f.readline().split())
+        adjacency_list[u].append((v,w))
+        adjacency_list[v].append((u,w))
+
+    key, parent = primsAlgorithm(adjacency_list, 0)
+    print(sum(key))
+
+    # print(len(adjacency_list))
+
+
+
+
+G = nx.Graph()
+
+for u in range(n):
+    if parent[u] != -1:
+        G.add_edge(parent[u], u, weight = key[u])
+
+pos = nx.spring_layout(G)
+nx.draw(G, pos, with_labels = True, node_size = 10, node_color='lightblue', font_size=10)
+edge_labels = nx.get_edge_attributes(G, 'weight')
+plt.show()
